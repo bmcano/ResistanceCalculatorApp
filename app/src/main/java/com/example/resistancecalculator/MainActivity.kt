0a54702018555ee5a7887a2445bf81ec4b5381b5
Brandon Cano <brandoncano01@gmail.com>
2022-06-08 22:37:55 -0500

    +Added color images for better visualization ~UI redesign

--- app/src/main/java/com/example/resistancecalculator/MainActivity.kt
+++ app/src/main/java/com/example/resistancecalculator/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.resistancecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
+    private lateinit var colorImage_1: ImageView
+    private lateinit var colorImage_2: ImageView
+    private lateinit var colorImage_3: ImageView
+    private lateinit var colorImage_4: ImageView
 
+    // initialize empty strings
     private var colorBand1: String = ""
     private var colorBand2: String = ""
     private var multiplierBand: String = ""
@@ -31,6 +33,11 @@ class MainActivity : AppCompatActivity() {
     private fun setUp() {
         // shows the calculated resistance
         textView = findViewById(R.id.resistance_display)
+
+        colorImage_1 = findViewById(R.id.color_image_1)
+        colorImage_2 = findViewById(R.id.color_image_2)
+        colorImage_3 = findViewById(R.id.color_image_3)
+        colorImage_4 = findViewById(R.id.color_image_4)
 
         // color band 1
         val spinner1: Spinner = findViewById(R.id.color_band_1)
@@ -69,19 +76,48 @@ class MainActivity : AppCompatActivity() {
                 id: Long
             ) {
                 when(parent!!.id) {
-                    R.id.color_band_1 -> colorBand1 = parent.getItemAtPosition(position).toString()
-                    R.id.color_band_2 -> colorBand2 = parent.getItemAtPosition(position).toString()
-                    R.id.multiplier_band-> multiplierBand = parent.getItemAtPosition(position).toString()
-                    R.id.tolerance_band -> toleranceBand = parent.getItemAtPosition(position).toString()
-                    else -> { }
+                    R.id.color_band_1 -> {
+                        colorBand1 = parent.getItemAtPosition(position).toString()
+                        colorImage_1.setImageResource(imageColor(colorBand1))
+                    }
+                    R.id.color_band_2 -> {
+                        colorBand2 = parent.getItemAtPosition(position).toString()
+                        colorImage_2.setImageResource(imageColor(colorBand2))
+                    }
+                    R.id.multiplier_band -> {
+                        multiplierBand = parent.getItemAtPosition(position).toString()
+                        colorImage_3.setImageResource(imageColor(multiplierBand))
+                    }
+                    R.id.tolerance_band -> {
+                        toleranceBand = parent.getItemAtPosition(position).toString()
+                        colorImage_4.setImageResource(imageColor(toleranceBand))
+                    }
                 }
-                // colorBand1 = parent!!.getItemAtPosition(position).toString()
                 textView.text = calcResistance(colorBand1, colorBand2, multiplierBand, toleranceBand)
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 // can leave this empty
             }
+        }
+    }
+
+    // take the color name from the string arrays and finds the correct id
+    private fun imageColor(color: String): Int{
+        return when(color) {
+            "Black" -> R.drawable.black
+            "Blue" -> R.drawable.blue
+            "Brown" -> R.drawable.brown
+            "Gold" -> R.drawable.gold
+            "Gray" -> R.drawable.gray
+            "Green" -> R.drawable.green
+            "Orange" -> R.drawable.orange
+            "Red" -> R.drawable.red
+            "Silver" -> R.drawable.silver
+            "Violet" -> R.drawable.violet
+            "White" -> R.drawable.white
+            "Yellow" -> R.drawable.yellow
+            else -> { R.drawable.black }
         }
     }
 }
