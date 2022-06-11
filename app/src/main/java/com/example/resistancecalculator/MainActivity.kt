ef96453ced90571c11bc1d59e6f03de9365a8beb
Brandon Cano <brandoncano01@gmail.com>
2022-06-10 22:56:53 -0500

    #2 - removed legacy UI, and refactored new material

--- app/src/main/java/com/example/resistancecalculator/MainActivity.kt
+++ app/src/main/java/com/example/resistancecalculator/MainActivity.kt
@@ -3,37 +3,23 @@ package com.example.resistancecalculator
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    // Toast.makeText(this@MainActivity, "${parent!!.id}", Toast.LENGTH_SHORT).show()
-
     private lateinit var textView: TextView
-    private lateinit var colorImage1: ImageView
-    private lateinit var colorImage2: ImageView
-    private lateinit var colorImage3: ImageView
-    private lateinit var colorImage4: ImageView
 
-    // initialize empty strings
+    // initialize as empty strings
     private var numberBand1: String = ""
     private var numberBand2: String = ""
     private var multiplierBand: String = ""
     private var toleranceBand: String = ""
 
-    private var newLayout = true
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if(newLayout) {
-            setContentView(R.layout.activity_main_new)
-            setUpNew()
-        } else {
-            setContentView(R.layout.activity_main)
-            setUp()
-        }
+        setContentView(R.layout.activity_main)
+        setup()
 
         // sets the action bar to @color/purple_500
         val actionBar: ActionBar? = supportActionBar
@@ -43,16 +29,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        if(newLayout) {
-            setUpNew()
-        } else {
-            setUp()
-        }
+        setup()
     }
 
 
-    // new stuff for working on the new UI, will replace the old bits once it is functioning
-    private fun setUpNew() {
+    // sets up all drop down menus, and the rest of the screen
+    private fun setup() {
         // will display the resistance
         textView = findViewById(R.id.resistance_display_new)
 
@@ -133,84 +115,6 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-
-    /**
-     *  DO NOT CHANGE BELOW: this is the original working UI, new stuff is still a work in progress
-     */
-
-    // initial setup method
-    private fun setUp() {
-        // shows the calculated resistance
-        textView = findViewById(R.id.resistance_display)
-
-        colorImage1 = findViewById(R.id.color_image_1)
-        colorImage2 = findViewById(R.id.color_image_2)
-        colorImage3 = findViewById(R.id.color_image_3)
-        colorImage4 = findViewById(R.id.color_image_4)
-
-        // color band 1
-        val spinner1: Spinner = findViewById(R.id.color_band_1)
-        setSpinner(spinner1, R.array.color_bands)
-
-        // color band 2
-        val spinner2: Spinner = findViewById(R.id.color_band_2)
-        setSpinner(spinner2, R.array.color_bands)
-
-        // multiplier band
-        val spinner3: Spinner = findViewById(R.id.multiplier_band)
-        setSpinner(spinner3, R.array.multiplier_band)
-
-        // tolerance band
-        val spinner4: Spinner = findViewById(R.id.tolerance_band)
-        setSpinner(spinner4, R.array.tolerance_band)
-    }
-
-    // private method to create and set the adapters for each spinner,
-    // also includes the onItemSelectedListener
-    private fun setSpinner(spinner: Spinner, array: Int) {
-        ArrayAdapter.createFromResource(
-            this,
-            array,
-            android.R.layout.simple_spinner_dropdown_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) // simple_spinner_item
-            spinner.adapter = adapter
-        }
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
-                when(parent!!.id) {
-                    R.id.color_band_1 -> {
-                        numberBand1 = parent.getItemAtPosition(position).toString()
-                        colorImage1.setImageResource(imageColor(numberBand1))
-                    }
-                    R.id.color_band_2 -> {
-                        numberBand2 = parent.getItemAtPosition(position).toString()
-                        colorImage2.setImageResource(imageColor(numberBand2))
-                    }
-                    R.id.multiplier_band -> {
-                        multiplierBand = parent.getItemAtPosition(position).toString()
-                        colorImage3.setImageResource(imageColor(multiplierBand))
-                    }
-                    R.id.tolerance_band -> {
-                        toleranceBand = parent.getItemAtPosition(position).toString()
-                        colorImage4.setImageResource(imageColor(toleranceBand))
-                    }
-                }
-                textView.text = calcResistance(numberBand1, numberBand2, multiplierBand, toleranceBand)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                // can leave this empty
-            }
-        }
-    }
-
     // take the color name from the string arrays and finds the correct id
     private fun imageColor(color: String): Int {
         return when(color) {
@@ -226,7 +130,7 @@ class MainActivity : AppCompatActivity() {
             "Violet" -> R.drawable.violet32
             "White" -> R.drawable.white32
             "Yellow" -> R.drawable.yellow32
-            else -> { R.drawable.black32 }
+            else -> { R.drawable.blank32 }
         }
     }
 }
