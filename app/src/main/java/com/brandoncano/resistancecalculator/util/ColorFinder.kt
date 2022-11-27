8eda53f84fd3de0482563ee0914f96f0273904c5
Brandon Cano <brandoncano01@gmail.com>
2022-11-26 22:41:11 -0600

    #16 - Added ColorFinder unit testing, ignore the randomColor method

--- app/src/main/java/com/brandoncano/resistancecalculator/util/ColorFinder.kt
+++ app/src/main/java/com/brandoncano/resistancecalculator/util/ColorFinder.kt
@@ -35,18 +35,18 @@ object ColorFinder {
     // note: "${PLUS_MINUS}20%" -> R.color.resistor_blank
     fun textToColor(color: String = ""): Int {
         return when (color) {
+            "Black" ,                       "250 ppm/${DEGREE}C" -> R.color.black32
+            "Brown" , "${PLUS_MINUS}1%"   , "100 ppm/${DEGREE}C" -> R.color.brown32
             "Red"   , "${PLUS_MINUS}2%"   , "50 ppm/${DEGREE}C"  -> R.color.red32
             "Orange",                       "15 ppm/${DEGREE}C"  -> R.color.orange32
-            "Yellow"                                             -> R.color.yellow32
-            "Gold"  , "${PLUS_MINUS}5%"   , "25 ppm/${DEGREE}C"  -> R.color.gold32
+            "Yellow",                       "25 ppm/${DEGREE}C"  -> R.color.yellow32
             "Green" , "${PLUS_MINUS}0.5%" , "20 ppm/${DEGREE}C"  -> R.color.green32
             "Blue"  , "${PLUS_MINUS}0.25%", "10 ppm/${DEGREE}C"  -> R.color.blue32
             "Violet", "${PLUS_MINUS}0.1%" , "5 ppm/${DEGREE}C"   -> R.color.violet32
+            "Gray"  , "${PLUS_MINUS}0.05%", "1 ppm/${DEGREE}C"   -> R.color.gray32
             "White"                                              -> R.color.white32
             "Silver", "${PLUS_MINUS}10%"                         -> R.color.silver32
-            "Gray"  , "${PLUS_MINUS}0.05%", "1 ppm/${DEGREE}C"   -> R.color.gray32
-            "Black" ,                       "250 ppm/${DEGREE}C" -> R.color.black32
-            "Brown" , "${PLUS_MINUS}1%"   , "100 ppm/${DEGREE}C" -> R.color.brown32
+            "Gold"  , "${PLUS_MINUS}5%"                          -> R.color.gold32
             else                                                 -> R.color.resistor_blank
         }
     }
@@ -80,6 +80,7 @@ object ColorFinder {
             R.color.blue32, R.drawable.blue_square -> "Blue"
             R.color.violet32, R.drawable.violet_square -> "Violet"
             R.color.gray32, R.drawable.gray_square -> "Gray"
+            R.color.white32, R.drawable.white_square -> "White"
             R.color.gold32, R.drawable.gold_square -> "Gold"
             R.color.silver32, R.drawable.silver_square -> "Silver"
             else -> EMPTY_STRING
---
+++ app/src/test/java/com/brandoncano/resistancecalculator/util/ColorFinderTest.kt
@@ -0,0 +1,171 @@
+package com.brandoncano.resistancecalculator.util
+
+import com.brandoncano.resistancecalculator.R
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * @author Brandon
+ */
+class ColorFinderTest {
+
+    private companion object {
+        const val PLUS_MINUS: String = "±"
+        const val DEGREE: String = "°"
+    }
+
+    @Test
+    fun textToColorDrawableTest() {
+        assertEquals(R.drawable.black_square, ColorFinder.textToColoredDrawable("Black"))
+        assertEquals(R.drawable.black_square, ColorFinder.textToColoredDrawable("250 ppm/${DEGREE}C"))
+
+        assertEquals(R.drawable.brown_square, ColorFinder.textToColoredDrawable("Brown"))
+        assertEquals(R.drawable.brown_square, ColorFinder.textToColoredDrawable("${PLUS_MINUS}1%"))
+        assertEquals(R.drawable.brown_square, ColorFinder.textToColoredDrawable("100 ppm/${DEGREE}C"))
+
+        assertEquals(R.drawable.red_square, ColorFinder.textToColoredDrawable("Red"))
+        assertEquals(R.drawable.red_square, ColorFinder.textToColoredDrawable("${PLUS_MINUS}2%"))
+        assertEquals(R.drawable.red_square, ColorFinder.textToColoredDrawable("50 ppm/${DEGREE}C"))
+
+        assertEquals(R.drawable.orange_square, ColorFinder.textToColoredDrawable("Orange"))
+        assertEquals(R.drawable.orange_square, ColorFinder.textToColoredDrawable("15 ppm/${DEGREE}C"))
+
+        assertEquals(R.drawable.yellow_square, ColorFinder.textToColoredDrawable("Yellow"))
+        assertEquals(R.drawable.yellow_square, ColorFinder.textToColoredDrawable("25 ppm/${DEGREE}C"))
+
+        assertEquals(R.drawable.green_square, ColorFinder.textToColoredDrawable("Green"))
+        assertEquals(R.drawable.green_square, ColorFinder.textToColoredDrawable("${PLUS_MINUS}0.5%"))
+        assertEquals(R.drawable.green_square, ColorFinder.textToColoredDrawable("20 ppm/${DEGREE}C"))
+
+        assertEquals(R.drawable.blue_square, ColorFinder.textToColoredDrawable("Blue"))
+        assertEquals(R.drawable.blue_square, ColorFinder.textToColoredDrawable("${PLUS_MINUS}0.25%"))
+        assertEquals(R.drawable.blue_square, ColorFinder.textToColoredDrawable("10 ppm/${DEGREE}C"))
+
+        assertEquals(R.drawable.violet_square, ColorFinder.textToColoredDrawable("Violet"))
+        assertEquals(R.drawable.violet_square, ColorFinder.textToColoredDrawable("${PLUS_MINUS}0.1%"))
+        assertEquals(R.drawable.violet_square, ColorFinder.textToColoredDrawable("5 ppm/${DEGREE}C"))
+
+        assertEquals(R.drawable.gray_square, ColorFinder.textToColoredDrawable("Gray"))
+        assertEquals(R.drawable.gray_square, ColorFinder.textToColoredDrawable("${PLUS_MINUS}0.05%"))
+        assertEquals(R.drawable.gray_square, ColorFinder.textToColoredDrawable("1 ppm/${DEGREE}C"))
+
+        assertEquals(R.drawable.white_square, ColorFinder.textToColoredDrawable("White"))
+
+        assertEquals(R.drawable.gold_square, ColorFinder.textToColoredDrawable("Gold"))
+        assertEquals(R.drawable.gold_square, ColorFinder.textToColoredDrawable("${PLUS_MINUS}5%"))
+
+        assertEquals(R.drawable.silver_square, ColorFinder.textToColoredDrawable("Silver"))
+        assertEquals(R.drawable.silver_square, ColorFinder.textToColoredDrawable("${PLUS_MINUS}10%"))
+
+        assertEquals(R.drawable.blank32, ColorFinder.textToColoredDrawable())
+        assertEquals(R.drawable.blank32, ColorFinder.textToColoredDrawable("Some String"))
+        assertEquals(R.drawable.blank32, ColorFinder.textToColoredDrawable("Not a color Red"))
+    }
+
+    @Test
+    fun textToColorTest() {
+        assertEquals(R.color.black32, ColorFinder.textToColor("Black"))
+        assertEquals(R.color.black32, ColorFinder.textToColor("250 ppm/${DEGREE}C"))
+
+        assertEquals(R.color.brown32, ColorFinder.textToColor("Brown"))
+        assertEquals(R.color.brown32, ColorFinder.textToColor("${PLUS_MINUS}1%"))
+        assertEquals(R.color.brown32, ColorFinder.textToColor("100 ppm/${DEGREE}C"))
+
+        assertEquals(R.color.red32, ColorFinder.textToColor("Red"))
+        assertEquals(R.color.red32, ColorFinder.textToColor("${PLUS_MINUS}2%"))
+        assertEquals(R.color.red32, ColorFinder.textToColor("50 ppm/${DEGREE}C"))
+
+        assertEquals(R.color.orange32, ColorFinder.textToColor("Orange"))
+        assertEquals(R.color.orange32, ColorFinder.textToColor("15 ppm/${DEGREE}C"))
+
+        assertEquals(R.color.yellow32, ColorFinder.textToColor("Yellow"))
+        assertEquals(R.color.yellow32, ColorFinder.textToColor("25 ppm/${DEGREE}C"))
+
+        assertEquals(R.color.green32, ColorFinder.textToColor("Green"))
+        assertEquals(R.color.green32, ColorFinder.textToColor("${PLUS_MINUS}0.5%"))
+        assertEquals(R.color.green32, ColorFinder.textToColor("20 ppm/${DEGREE}C"))
+
+        assertEquals(R.color.blue32, ColorFinder.textToColor("Blue"))
+        assertEquals(R.color.blue32, ColorFinder.textToColor("${PLUS_MINUS}0.25%"))
+        assertEquals(R.color.blue32, ColorFinder.textToColor("10 ppm/${DEGREE}C"))
+
+        assertEquals(R.color.violet32, ColorFinder.textToColor("Violet"))
+        assertEquals(R.color.violet32, ColorFinder.textToColor("${PLUS_MINUS}0.1%"))
+        assertEquals(R.color.violet32, ColorFinder.textToColor("5 ppm/${DEGREE}C"))
+
+        assertEquals(R.color.gray32, ColorFinder.textToColor("Gray"))
+        assertEquals(R.color.gray32, ColorFinder.textToColor("${PLUS_MINUS}0.05%"))
+        assertEquals(R.color.gray32, ColorFinder.textToColor("1 ppm/${DEGREE}C"))
+
+        assertEquals(R.color.white32, ColorFinder.textToColor("White"))
+
+        assertEquals(R.color.gold32, ColorFinder.textToColor("Gold"))
+        assertEquals(R.color.gold32, ColorFinder.textToColor("${PLUS_MINUS}5%"))
+
+        assertEquals(R.color.silver32, ColorFinder.textToColor("Silver"))
+        assertEquals(R.color.silver32, ColorFinder.textToColor("${PLUS_MINUS}10%"))
+
+        assertEquals(R.color.resistor_blank, ColorFinder.textToColor())
+        assertEquals(R.color.resistor_blank, ColorFinder.textToColor("Some String"))
+        assertEquals(R.color.resistor_blank, ColorFinder.textToColor("Not a color Red"))
+    }
+
+    @Test
+    fun numberToColorTest() {
+        assertEquals(R.color.black32, ColorFinder.numberToColor(0))
+        assertEquals(R.color.brown32, ColorFinder.numberToColor(1))
+        assertEquals(R.color.red32, ColorFinder.numberToColor(2))
+        assertEquals(R.color.orange32, ColorFinder.numberToColor(3))
+        assertEquals(R.color.yellow32, ColorFinder.numberToColor(4))
+        assertEquals(R.color.green32, ColorFinder.numberToColor(5))
+        assertEquals(R.color.blue32, ColorFinder.numberToColor(6))
+        assertEquals(R.color.violet32, ColorFinder.numberToColor(7))
+        assertEquals(R.color.gray32, ColorFinder.numberToColor(8))
+        assertEquals(R.color.white32, ColorFinder.numberToColor(9))
+
+        assertEquals(R.color.resistor_blank, ColorFinder.numberToColor(-1))
+        assertEquals(R.color.resistor_blank, ColorFinder.numberToColor())
+    }
+
+    @Test
+    fun idToColorTextTest() {
+        assertEquals("Black", ColorFinder.idToColorText(R.color.black32))
+        assertEquals("Black", ColorFinder.idToColorText(R.drawable.black_square))
+
+        assertEquals("Brown", ColorFinder.idToColorText(R.color.brown32))
+        assertEquals("Brown", ColorFinder.idToColorText(R.drawable.brown_square))
+
+        assertEquals("Red", ColorFinder.idToColorText(R.color.red32))
+        assertEquals("Red", ColorFinder.idToColorText(R.drawable.red_square))
+
+        assertEquals("Orange", ColorFinder.idToColorText(R.color.orange32))
+        assertEquals("Orange", ColorFinder.idToColorText(R.drawable.orange_square))
+
+        assertEquals("Yellow", ColorFinder.idToColorText(R.color.yellow32))
+        assertEquals("Yellow", ColorFinder.idToColorText(R.drawable.yellow_square))
+
+        assertEquals("Green", ColorFinder.idToColorText(R.color.green32))
+        assertEquals("Green", ColorFinder.idToColorText(R.drawable.green_square))
+
+        assertEquals("Blue", ColorFinder.idToColorText(R.color.blue32))
+        assertEquals("Blue", ColorFinder.idToColorText(R.drawable.blue_square))
+
+        assertEquals("Violet", ColorFinder.idToColorText(R.color.violet32))
+        assertEquals("Violet", ColorFinder.idToColorText(R.drawable.violet_square))
+
+        assertEquals("Gray", ColorFinder.idToColorText(R.color.gray32))
+        assertEquals("Gray", ColorFinder.idToColorText(R.drawable.gray_square))
+
+        assertEquals("White", ColorFinder.idToColorText(R.color.white32))
+        assertEquals("White", ColorFinder.idToColorText(R.drawable.white_square))
+
+        assertEquals("Silver", ColorFinder.idToColorText(R.color.silver32))
+        assertEquals("Silver", ColorFinder.idToColorText(R.drawable.silver_square))
+
+        assertEquals("Gold", ColorFinder.idToColorText(R.color.gold32))
+        assertEquals("Gold", ColorFinder.idToColorText(R.drawable.gold_square))
+
+        assertEquals("", ColorFinder.idToColorText(R.color.resistor_blank))
+        assertEquals("", ColorFinder.idToColorText(R.drawable.blank32))
+    }
+}
