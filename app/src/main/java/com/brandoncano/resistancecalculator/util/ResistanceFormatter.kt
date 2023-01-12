4dacba132a70843c50771fe924d744ccca5af95c
Brandon Cano <brandoncano01@gmail.com>
2023-01-11 22:54:53 -0600

    added usage of constants

--- app/src/main/java/com/brandoncano/resistancecalculator/util/ResistanceFormatter.kt
+++ app/src/main/java/com/brandoncano/resistancecalculator/util/ResistanceFormatter.kt
@@ -1,9 +1,7 @@
 package com.brandoncano.resistancecalculator.util
 
 import com.brandoncano.resistancecalculator.components.Resistor
-import com.brandoncano.resistancecalculator.constants.OHMS
-import com.brandoncano.resistancecalculator.constants.PLUS_MINUS
-import com.brandoncano.resistancecalculator.constants.PPM_UNIT
+import com.brandoncano.resistancecalculator.constants.*
 
 /**
  * Job: Formats the resistance based on the colors selected for the bands (CtV).
@@ -11,26 +9,25 @@ import com.brandoncano.resistancecalculator.constants.PPM_UNIT
 object ResistanceFormatter {
 
     private val colorToNumber = mapOf(
-        "Black" to "0", "Brown" to "1", "Red" to "2", "Orange" to "3", "Yellow" to "4",
-        "Green" to "5", "Blue" to "6", "Violet" to "7", "Gray" to "8", "White" to "9"
+        BLACK to "0", BROWN to "1", RED to "2", ORANGE to "3", YELLOW to "4",
+        GREEN to "5", BLUE to "6", VIOLET to "7", GRAY to "8", WHITE to "9"
     )
 
     private val colorToMultiplier = mapOf(
-        "Black" to "1 ", "Brown" to "10 ", "Red" to "100 ",
-        "Orange" to "1 k", "Yellow" to "10 k", "Green" to "100 k",
-        "Blue" to "1 M", "Violet" to "10 M", "Gray" to "100 M",
-        "White" to "1 G", "Gold" to "0.1 ", "Silver" to "0.01 "
+        BLACK to "1 ", BROWN to "10 ", RED to "100 ",
+        ORANGE to "1 k", YELLOW to "10 k", GREEN to "100 k",
+        BLUE to "1 M", VIOLET to "10 M", GRAY to "100 M",
+        WHITE to "1 G", GOLD to "0.1 ", SILVER to "0.01 "
     )
 
     private val colorToTolerance = mapOf(
-        "Brown" to "1%", "Red" to "2%", "Green" to "0.5%", "Blue" to "0.25%",
-        "Violet" to "0.1%", "Gray" to "0.05%", "Gold" to "5%", "Silver" to "10%"
+        BROWN to "1%", RED to "2%", GREEN to "0.5%", BLUE to "0.25%",
+        VIOLET to "0.1%", GRAY to "0.05%", GOLD to "5%", SILVER to "10%"
     )
 
     private val colorToPPM = mapOf(
-        "Black" to "\n250", "Brown" to "\n100", "Red" to "\n50",
-        "Orange" to "\n15", "Yellow" to "\n25", "Green" to "\n20",
-        "Blue" to "\n10", "Violet" to "\n5", "Gray" to "\n1"
+        BLACK to "\n250", BROWN to "\n100", RED to "\n50", ORANGE to "\n15",
+        YELLOW to "\n25", GREEN to "\n20", BLUE to "\n10", VIOLET to "\n5", GRAY to "\n1"
     )
 
     // works for all 4, 5, and 6 band resistors
@@ -55,18 +52,18 @@ object ResistanceFormatter {
     // four band resistor
     private fun formatMultiplier(color: String, band1: String, band2: String): String {
         return when (color) {
-            "Black" -> "$band1$band2 "
-            "Brown" -> "$band1${band2}0 "
-            "Red" -> "${band1}.${band2} k"
-            "Orange" -> "$band1${band2} k"
-            "Yellow" -> "$band1${band2}0 k"
-            "Green" -> "${band1}.${band2} M"
-            "Blue" -> "$band1${band2} M"
-            "Violet" -> "$band1${band2}0 M"
-            "Gray" -> "${band1}.${band2} G"
-            "White" -> "$band1${band2} G"
-            "Gold" -> "${band1}.${band2} "
-            "Silver" -> "0.$band1${band2} "
+            BLACK -> "$band1$band2 "
+            BROWN -> "$band1${band2}0 "
+            RED -> "${band1}.${band2} k"
+            ORANGE -> "$band1${band2} k"
+            YELLOW -> "$band1${band2}0 k"
+            GREEN -> "${band1}.${band2} M"
+            BLUE -> "$band1${band2} M"
+            VIOLET -> "$band1${band2}0 M"
+            GRAY -> "${band1}.${band2} G"
+            WHITE -> "$band1${band2} G"
+            GOLD -> "${band1}.${band2} "
+            SILVER -> "0.$band1${band2} "
             else -> "$band1$band2 "
         }
     }
@@ -74,18 +71,18 @@ object ResistanceFormatter {
     // five or six band resistor
     private fun formatMultiplier(color: String, band1: String, band2: String, band3: String): String {
         return band1 + when (color) {
-            "Black" -> "$band2$band3 "
-            "Brown" -> ".$band2$band3 k"
-            "Red" -> "${band2}.$band3 k"
-            "Orange" -> "$band2$band3 k"
-            "Yellow" -> ".$band2$band3 M"
-            "Green" -> "${band2}.$band3 M"
-            "Blue" -> "$band2$band3 M"
-            "Violet" -> ".$band2$band3 G"
-            "Gray" -> "${band2}.$band3 G"
-            "White" -> "$band2$band3 G"
-            "Gold" -> "$band2.$band3 "
-            "Silver" -> ".$band2$band3 "
+            BLACK -> "$band2$band3 "
+            BROWN -> ".$band2$band3 k"
+            RED -> "${band2}.$band3 k"
+            ORANGE -> "$band2$band3 k"
+            YELLOW -> ".$band2$band3 M"
+            GREEN -> "${band2}.$band3 M"
+            BLUE -> "$band2$band3 M"
+            VIOLET -> ".$band2$band3 G"
+            GRAY -> "${band2}.$band3 G"
+            WHITE -> "$band2$band3 G"
+            GOLD -> "$band2.$band3 "
+            SILVER -> ".$band2$band3 "
             else -> "$band2$band3 "
         }
     }
@@ -116,16 +113,16 @@ object ResistanceFormatter {
 
         // format multiplier for edge cases of leading 0s
         val (multiple, unit) = colorToMultiplier.getValue(resistor.multiplierBand).split(" ")
-        if (resistor.getNumberOfBands() != 4 && resistor.sigFigBandOne == "Black" && resistor.sigFigBandTwo != "Black") {
+        if (resistor.getNumberOfBands() != 4 && resistor.sigFigBandOne == BLACK && resistor.sigFigBandTwo != BLACK) {
             multiplier = when (resistor.multiplierBand) {
-                "Brown" -> "${sigFigTwo}${sigFigThree}0 "
-                "Yellow" -> "${sigFigTwo}${sigFigThree}0 k"
-                "Violet" -> "${sigFigTwo}${sigFigThree}0 M"
-                "Silver" -> "0.${sigFigTwo}${sigFigThree} "
+                BROWN -> "${sigFigTwo}${sigFigThree}0 "
+                YELLOW -> "${sigFigTwo}${sigFigThree}0 k"
+                VIOLET -> "${sigFigTwo}${sigFigThree}0 M"
+                SILVER -> "0.${sigFigTwo}${sigFigThree} "
                 else -> formatMultiplier(resistor.multiplierBand, "", sigFigTwo, sigFigThree)
             }
-        } else if (resistor.sigFigBandOne == "Black") {
-            multiplier = if (resistor.multiplierBand == "Silver") {
+        } else if (resistor.sigFigBandOne == BLACK) {
+            multiplier = if (resistor.multiplierBand == SILVER) {
                 String.format("%.2f", (value * multiple.toFloat()))
             } else {
                 String.format("%.1f", (value * multiple.toFloat()))
