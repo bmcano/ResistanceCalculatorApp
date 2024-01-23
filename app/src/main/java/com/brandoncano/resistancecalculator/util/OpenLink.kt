10047249d0a5aa6a3b2d5ab623d0f603371be717
Brandon Cano <brandoncano01@gmail.com>
2024-01-23 17:15:24 -0600

    Crash fix(?): adding a try catch and new system for opening web links to avoid the securityException crash

--- app/src/main/java/com/brandoncano/resistancecalculator/util/OpenLink.kt
+++ app/src/main/java/com/brandoncano/resistancecalculator/util/OpenLink.kt
@@ -1,9 +1,9 @@
 package com.brandoncano.resistancecalculator.util
 
+import android.app.AlertDialog
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
-import androidx.core.content.ContextCompat.startActivity
+import androidx.browser.customtabs.CustomTabsIntent
 
 object OpenLink {
 
@@ -12,20 +12,29 @@ object OpenLink {
     private const val iecLink = "https://eepower.com/resistor-guide/resistor-standards-and-codes/resistor-color-code/#"
 
     fun openCapacitorApp(context: Context) {
-        val uri = Uri.parse(capacitorPlayStoreLink)
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        startActivity(context, intent, null)
+        open(context, capacitorPlayStoreLink)
     }
 
     fun openResistorApp(context: Context) {
-        val uri = Uri.parse(resistorPlayStoreLink)
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        startActivity(context, intent, null)
+        open(context, resistorPlayStoreLink)
     }
 
     fun openIECWebpage(context: Context) {
-        val uri = Uri.parse(iecLink)
-        val intent = Intent(Intent.ACTION_VIEW, uri)
-        startActivity(context, intent, null)
+        open(context, iecLink)
+    }
+
+    private fun open(context: Context, link: String) {
+        try {
+            val builder = CustomTabsIntent.Builder()
+            val customTabsIntent = builder.build()
+            customTabsIntent.launchUrl(context, Uri.parse(link))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Error")
+                .setMessage("A problem occurred while trying to open the link.")
+                .setPositiveButton("OK", null)
+                .show()
+        }
     }
 }
