96c6b287713efacd2e585c4f383de9f7371581c8
Brandon Cano <brandoncano01@gmail.com>
2024-07-09 14:25:23 -0500

    added ktlint plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.jetbrainsKotlin) apply false
+    alias(libs.plugins.gradleKtlint)
 }
