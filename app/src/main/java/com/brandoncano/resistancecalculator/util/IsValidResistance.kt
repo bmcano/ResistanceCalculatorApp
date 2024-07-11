add9273d7f0c8a1c2851210b9634ce359f625bcd
Brandon Cano <brandoncano01@gmail.com>
2024-07-11 11:50:53 -0500

    Crash Fix: NumberFormatException - spaces were making their way through the input filters

--- app/src/main/java/com/brandoncano/resistancecalculator/util/IsValidResistance.kt
+++ app/src/main/java/com/brandoncano/resistancecalculator/util/IsValidResistance.kt
@@ -8,7 +8,7 @@ import com.brandoncano.resistancecalculator.model.vtc.ResistorVtc
  * Job: Checks if a user inputted resistance is valid.
  *
  * Notes:
- *   EditText already limits this to decimal and whole numbers and a max of 5 characters.
+ *
  *   Invalid inputs (where [x,y,z,w] are some numeric digit):
  *    - 4 Bands: xyz, 0xy, 00x; .x, .xy, .xyz; 0.0, 0.00x, 0.0xy, x.0y, x.0yz; xy0 for GΩ
  *    - 5/6 Bands: xyzw, 0xy(z), 00x(y); .xy, .0x; 0.xyz, 0.0xyz, x.0yz, x.y0z, 0.00x; 1230 for GΩ
@@ -20,8 +20,14 @@ object IsValidResistance {
 
     fun execute(resistor: ResistorVtc, input: String): Boolean {
         if (input.isEmpty()) return true
-        input.toDoubleOrNull() ?: return false
-        val sigFigs = SignificantFigures(input).numberSignificantFigures
+        if (input.contains(" ")) return false
+        val sigFigs: Int
+        try {
+            sigFigs = SignificantFigures(input).numberSignificantFigures
+        } catch (e: NumberFormatException) {
+            return false
+        }
+
         val isThreeFourBand = resistor.isThreeFourBand()
         val units = resistor.units
 
