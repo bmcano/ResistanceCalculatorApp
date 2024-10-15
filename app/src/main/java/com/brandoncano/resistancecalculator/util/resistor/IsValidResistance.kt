fc272671366631de4b1d5fe3ba2afcfb673da0ae
Brandon Cano <brandoncano01@gmail.com>
2024-10-14 19:39:03 -0500

    #83 - rewrote validation util to address issues and make more consistent formatting, unit test is likely wrong in some spots now

--- app/src/main/java/com/brandoncano/resistancecalculator/util/resistor/IsValidResistance.kt
+++ app/src/main/java/com/brandoncano/resistancecalculator/util/resistor/IsValidResistance.kt
@@ -1,25 +1,45 @@
 package com.brandoncano.resistancecalculator.util.resistor
 
 import com.Ostermiller.util.SignificantFigures
-import com.brandoncano.resistancecalculator.constants.Symbols.GOHMS
 import com.brandoncano.resistancecalculator.model.vtc.ResistorVtc
+import com.brandoncano.resistancecalculator.util.MultiplierFromUnits
 
 /**
  * Job: Checks if a user inputted resistance is valid.
  *
- * Notes:
- *   Invalid inputs (where [x,y,z,w] are some numeric digit):
- *    - 4 Bands: xyz, 0xy, 00x; .x, .xy, .xyz; 0.0, 0.00x, 0.0xy, x.0y, x.0yz; xy0 for GΩ
- *    - 5/6 Bands: xyzw, 0xy(z), 00x(y); .xy, .0x; 0.xyz, 0.0xyz, x.0yz, x.y0z, 0.00x; 1230 for GΩ
- *   Valid inputs (special cases):
- *    - 4 Bands: xy.0, 0.xy, x.y
- *    - 5/6 Bands: x.yz, xy.z, xyz.0
+ * Note: Valid inputs are written next to each Regex
  */
 object IsValidResistance {
+
+    private val validThreeFourBandPatterns = listOf(
+        Regex("""^\d\.?$"""),       // x0 or x.
+        Regex("""^\d\.\d$"""),      // x.y
+        Regex("""^\d\d$"""),        // xy or xy.0
+        Regex("""^0\.\d\d?$""")     // 0.xy or 0.x0
+    )
+
+    private val validFiveSixBandPatterns = listOf(
+        Regex("""^\d\.?$"""),       // x00 or x.
+        Regex("""^\d\d\.?$"""),     // xy0 or xy.
+        Regex("""^\d\.\d\d?$"""),   // x.yz or x.y0
+        Regex("""^\d\d\.\d$"""),    // xy.z
+        Regex("""^\d\d\d$"""),      // xyz
+        Regex("""^0\.\d\d\d?$""")   // 0.xyz or 0.xy0
+    )
 
     fun execute(resistor: ResistorVtc, input: String): Boolean {
         input.ifEmpty { return true }
         if (input.contains(" ")) return false
+        if (input.startsWith(".")) return false
+        if (input.length == 1 && input.startsWith("0")) return true
+        if (input.length == 2 && input.startsWith("0.")) return true
+        if (input.length == 2 && input.startsWith('0') && input[1] != '.') return false
+
+        val isThreeFourBand = resistor.isThreeFourBand()
+        val patterns = if (isThreeFourBand) validThreeFourBandPatterns else validFiveSixBandPatterns
+        val isValidFormat = patterns.any { it.matches(input) }
+        if (isValidFormat) return true
+
         val sigFigs: Int
         try {
             sigFigs = SignificantFigures(input).numberSignificantFigures
@@ -27,32 +47,11 @@ object IsValidResistance {
             return false
         }
 
-        val isThreeFourBand = resistor.isThreeFourBand()
-        val units = resistor.units
-
-        // check for specific conditions, priority matters
-        return when {
-            input.length > 1 && input[0] == '0' && input[1] != '.' ||
-            input.isNotEmpty() && input[0] == '.' -> false
-
-            isThreeFourBand -> when { /* THREE/FOUR BAND */
-                input.startsWith("0.00") ||
-                sigFigs >= 2 && input.startsWith("0.0") -> false
-                sigFigs == 3 && input.endsWith(".0") ||
-                sigFigs == 2 && input.startsWith("0.") ||
-                sigFigs <= 2 && '.' in input -> true
-                sigFigs > 2 || units == GOHMS && input.length > 2 -> false
-                else -> true
-            }
-            else -> when { /* FIVE/SIX BAND */
-                input.startsWith("0.00") -> false
-                sigFigs == 4 && input.endsWith(".0") -> true
-                sigFigs == 3 && input.startsWith("0") ||
-                sigFigs == 2 && input.startsWith("0.0") -> false
-                sigFigs <= 3 && '.' in input -> true
-                sigFigs > 3 || units == GOHMS && input.length > 3 -> false
-                else -> true
-            }
-        }
+        val maxSigFigs = if (isThreeFourBand) 2 else 3
+        if (sigFigs > maxSigFigs) return false
+        val maxValue = if (isThreeFourBand) 99_000_000_000 else 999_000_000_000
+        val resistance = input.toDoubleOrNull() ?: return false
+        val multiplier = MultiplierFromUnits.execute(resistor.units)
+        return ((resistance * multiplier) <= maxValue)
     }
 }
